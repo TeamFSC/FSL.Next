a398a7ebf291e98bb2c6b58f70419444d0db5ace
FST-GitRes <165374374+FST-GitRes@users.noreply.github.com>
2024-09-08 16:41:27 +0800

    添加项目文件。

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.IO;
+using iNKORE.UI.WPF.Modern;
+using Newtonsoft.Json;
+
+namespace FSL.Next
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Pages.Mainpage mainpage = new();
+        Pages.Accounts accounts = new();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            if( !File.Exists("./config/accounts.fsl") || !File.Exists("./config/settings.fsl" ) )
+            {
+                iNKORE.UI.WPF.Modern.Controls.MessageBox.Show("启动器检测到配置文件缺失，\n如果您是第一次使用，请先配置启动选项。\n感谢您选择FSL！", "欢迎使用FSL.Next！");
+                Directory.CreateDirectory("./config");
+
+                /* 
+                 * 有人说不写注释就很难理解
+                 * 此处判断配置文件是否存在，并且根据情况创建
+                 * 可以避免误杀另一个配置文件的情况！
+                */
+
+                if(!File.Exists("./config/accounts.fsl"))
+                {
+                    File.CreateText("./config/accounts.fsl");
+                }
+
+                if (!File.Exists("./config/settings.fsl"))
+                {
+                    File.CreateText("./config/settings.fsl");
+                }
+            }
+            
+        }
+
+        private void mainNav_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            nav.Content = new Frame()
+            {
+                Content = mainpage
+            };
+        }
+
+        private void accNav_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            nav.Content = new Frame()
+            {
+                Content = accounts
+            };
+        }
+    }
+}
---
+++ Pages/Accounts.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Forms;
+
+using Newtonsoft.Json;
+using System.IO;
+using iNKORE.UI.WPF.Modern;
+using System.Diagnostics;
+
+namespace FSL.Next.Pages
+{
+    /// <summary>
+    /// Accounts.xaml 的交互逻辑
+    /// </summary>
+    public partial class Accounts : Page
+    {
+        public Accounts()
+        {
+            InitializeComponent();
+            tips.Visibility = Visibility.Hidden;
+            offlineName.Visibility = Visibility.Collapsed;
+            msStart.Visibility = Visibility.Collapsed;
+        }
+
+        private void switchAuthType(int type)
+        {
+            switch (type)
+            {
+                case 0:
+                    offlineName.Visibility = Visibility.Visible;
+                    offlineName.Text = "Player_Name";
+                    msStart.Visibility = Visibility.Collapsed;
+                    break;
+                case 1:
+                    msStart.Visibility = Visibility.Visible;
+                    offlineName.Visibility = Visibility.Collapsed;
+                    break;
+            }
+        }
+
+        // 界面设计的最好看的一集！（大喜）
+
+        private void newAcc_Click(object sender, RoutedEventArgs e)
+        {
+            accInfo.Content = "如有能力，FSL建议您选择正版，毕竟89又不贵...";
+
+            offlineName.Visibility = Visibility.Collapsed;
+            msStart.Visibility = Visibility.Collapsed;
+            tips.Visibility = Visibility.Visible;
+            accConfig.Visibility = Visibility.Visible;
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            switchAuthType(authType.SelectedIndex);
+        }
+    }
+}
